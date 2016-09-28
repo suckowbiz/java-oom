57bd2de52e76d99363df832349656dbfa2a97969
Tobias Suckow <ize0j10@f-i-ts.de>
2016-09-28 06:17:18 +0200

    Have a little threshold before provoking OOM to enable administrative actions before HEAP is consumed.

--- src/main/java/biz/suckow/business/heap/control/HeapMonster.java
+++ src/main/java/biz/suckow/business/heap/control/HeapMonster.java
@@ -6,12 +6,15 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Singleton
 @Startup
 public class HeapMonster {
+    private static final long SLEEP_MILLIS = TimeUnit.SECONDS.toMillis(30);
 
     @Inject
     Logger logger;
@@ -20,7 +23,17 @@ public class HeapMonster {
     OOMService service;
 
     @PostConstruct
-    public void consume() {
+    public void wakeUpAndConsume() {
+        try {
+            logger.log(Level.INFO, "Sleeping for {0} s before provoking OutOfMemoryError.", TimeUnit.MILLISECONDS.toSeconds(SLEEP_MILLIS));
+            Thread.sleep(SLEEP_MILLIS);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "Interruption while sleeping: {0}.", e.getMessage());
+        }
+        this.consume();
+    }
+
+    private void consume() {
         logger.log(Level.INFO, "Going to provoke OutOfMemoryError. Now!");
         this.service.provokeOOM();
     }
